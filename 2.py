01f664344eb6e266eabc842669908f1d9e954475
shreyjain1994 <shrey.jain@mail.utoronto.ca>
2017-01-06 12:16:02 -0500

    problem 2 finished.

---
+++ 2.py
@@ -0,0 +1,27 @@
+"""
+Problem 2: Find sum of all even terms of the fibonacci sequence that do not exceed 4 million.
+"""
+
+
+def fib():
+    """
+    Generator of the terms in the fibonnaci sequence; 1,2,3,5,8,13
+    """
+    first = 1
+    second = 1
+
+    while True:
+        first, second = second, first + second
+        yield first
+
+
+fib = fib()
+total = 0
+while True:
+    current = next(fib)
+    if current > 4000000:
+        break
+    if current % 2 == 0:
+        total += current
+
+print(total)
