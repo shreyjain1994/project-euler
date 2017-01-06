4f0fea169b99572b8b9b7f10b98ae3394bcc9196
shreyjain1994 <shrey.jain@mail.utoronto.ca>
2017-01-06 15:02:31 -0500

    clean code. No need for variable to hold current prime number.

--- 7.py
+++ 7.py
@@ -5,8 +5,7 @@ Problem 7: What is the 10 001st prime number?
 from helpers import primes
 
 gen = primes()
-current = None
-for i in range(10001):
-    current = next(gen)
+for i in range(10000):
+    next(gen)
 
-print(current)
+print(next(gen))
