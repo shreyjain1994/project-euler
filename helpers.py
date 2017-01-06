e76ccc3a6942c3c5c79463490099801c0bac1219
shreyjain1994 <shrey.jain@mail.utoronto.ca>
2017-01-06 14:47:12 -0500

    Write a generator of prime numbers.

--- helpers.py
+++ helpers.py
@@ -18,3 +18,29 @@ def is_prime(n):
     upper_bound = int(math.sqrt(n))
 
     return not any(n % i == 0 for i in range(2, upper_bound + 1))
+
+
+def primes():
+    """
+    An unlimited generator of prime numbers: 2,3,5,7...
+
+    Usage:
+
+    >>> gen = primes()
+    >>> next(gen)
+    2
+    >>> next(gen)
+    3
+    >>> next(gen)
+    5
+    """
+
+    i = 1
+
+    while True:
+        found_prime = False
+        while not found_prime:
+            i += 1
+            found_prime = is_prime(i)
+        yield i
+
