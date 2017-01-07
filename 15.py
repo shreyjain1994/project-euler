d10f54c8d5aecfba04f1c5d266e6b11535f7652a
shreyjain1994 <shrey.jain@mail.utoronto.ca>
2017-01-06 21:49:25 -0500

    Clean code - more default values to make some checks in for loop unnecessary.

--- 15.py
+++ 15.py
@@ -7,20 +7,17 @@ Algorithm: Dynamic Programming
 # a 20x20 grid actually has 21 vertices on either side
 grid = [[None for i in range(21)] for j in range(21)]
 
-# default values - starting from any vertex on the bottom row, there is only 1 path
+# default values - starting from any vertex on the bottom row, there is only 1 path (going right all the way)
 for j in range(21):
     grid[20][j] = 1
 
+# default values - starting from any vertex on the far left column, there is only 1 path (going down all the way)
+for i in range(21):
+    grid[i][20] = 1
+
+# bottom up iterative code
 for i in range(19, -1, -1):
-    for j in range(20, -1, -1):
-
-        # paths if went down
-        num_of_paths = grid[i + 1][j]
-
-        # make sure there is space to go to the right
-        if j < 20:
-            num_of_paths += grid[i][j + 1]
-
-        grid[i][j] = num_of_paths
+    for j in range(19, -1, -1):
+        grid[i][j] = grid[i + 1][j] + grid[i][j + 1]
 
 print(grid[0][0])
